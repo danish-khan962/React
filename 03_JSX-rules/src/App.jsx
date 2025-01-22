014c37a27c23cda10ae7e934a90fdb16b4fcf2e5
Danish Khan <dk831675@gmail.com>
2025-01-22 08:13:44 +0530

    commit changes

---
+++ 03_JSX-rules/src/App.jsx
@@ -0,0 +1,11 @@
+import JSXRules from "./components/JSXRules"
+
+const App = () =>{
+  return (
+    <div>
+      <JSXRules/>
+    </div>
+  )
+}
+
+export default App
---
+++ 03_JSX-rules/src/components/JSXRules.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSX Rules</h1>
+      <p>JSX must return a <b>single parent element.</b></p>
+      <p>JSX elements must be <b>properly closed</b></p>
+      <p>JSX attributes are written using <b>camelCase</b> (e.g. className instead of class)</p>
+    </div>
+  )
+}
+
+export default JSXRules
